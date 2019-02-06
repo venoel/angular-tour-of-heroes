86ce0b499ff42e6edd6f96266437935601a9c501
Denis Olenev <denis.olenev@gmail.com>
2019-02-06 17:22:43 +0200

    New functionality - add hero.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -72,4 +72,12 @@ export class HeroService {
     );
   }
 
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
 }
