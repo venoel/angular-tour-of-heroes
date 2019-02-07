3d83f7021fed1bc92dff271b38e791362f10aa8d
Denis Olenev <denis.olenev@gmail.com>
2019-02-07 11:54:45 +0200

    Delete one hero.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -34,4 +34,9 @@ export class HeroesComponent implements OnInit {
       });
   }
 
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
 }
